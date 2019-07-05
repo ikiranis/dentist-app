a2c74008e539ffdce7310e5f8653ad26d3855d6c
rocean <rocean74@gmail.com>
2019-07-05 04:15:41 +0300

    Home vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid mt-3">
+
+    <div v-if="!isUserLoggedIn">
+      <Login v-if="!displayRegister && !username"/>
+      <div class="row">
+                <span class="btn btn-info my-3 ml-auto mr-auto" v-if="!displayRegister && !username"
+                      @click="displayRegisterComponent">Register User</span>
+      </div>
+    </div>
+
+    <display-bookmarks v-if="username" :searchId="searchId" :routeName="routeName" :textSearch="textSearch" />
+
+    <Register v-if="displayRegister"/>
+
+    <div class="row">
+      <span class="btn btn-info my-3 ml-auto mr-auto" v-if="displayRegister" @click="displayLoginComponent">Login User</span>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  // @ is an alias to /src
+  import api from '@/api';
+  import Login from '@/components/users/Login.vue'
+  import Register from '@/components/users/Register.vue'
+  import {mapState, mapMutations} from 'vuex';
+  import DisplayBookmarks from "@/components/bookmarks/DisplayBookmarks";
+  // TODO delete localStorage.accessToken if not valid
+  export default {
+    name: 'home',
+    components: {
+      DisplayBookmarks,
+      Login,
+      Register
+    },
+    data: () => ({
+      isUserLoggedIn: false
+    }),
+    computed: {
+      ...mapState(['displayRegister', 'username']),
+      textSearch: function () {
+        return this.$route.params.textSearch;
+      },
+      searchId: function () {
+        return this.$route.params.id;
+      },
+      routeName: function () {
+        return this.$route.name;
+      }
+    },
+    watch: {
+      username() {
+        // We make sure to take new value of localStorage.accessToken when username is changed
+        this.isUserLoggedIn = !!localStorage.accessToken;
+      }
+    },
+    mounted: function() {
+      this.isUserLoggedIn = !!localStorage.accessToken;
+    },
+    beforeCreate: function () {
+      api.getUsersCount()
+              .then(response => {
+                if (response.count === 0) {
+                  this.setDisplayRegister(true);
+                }
+              })
+              .catch(error => console.log(error.response));
+    },
+    methods: {
+      ...mapMutations(['setDisplayRegister']),
+      displayRegisterComponent() {
+        this.setDisplayRegister(true);
+      },
+      displayLoginComponent() {
+        this.setDisplayRegister(false);
+      }
+    }
   }
-}
 </script>
