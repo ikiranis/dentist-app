380de031870bf3d0400343c7a84bdae8dc77dfc3
rocean <rocean74@gmail.com>
2019-07-05 04:01:30 +0300

    Basic store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,62 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    username: null,
+    userId: 0,
+    displayRegister: false,
   },
   mutations: {
+    /**
+     * username setter
+     *
+     * @param state
+     * @param username
+     */
+    setUsername(state, username) {
+      state.username = username;
+    },
 
+    /**
+     * userId setter
+     *
+     * @param state
+     * @param userId
+     */
+    setUserId(state, userId) {
+      state.userId = userId;
+    },
+
+    /**
+     * displayRegister setter
+     *
+     * @param state
+     * @param value
+     */
+    setDisplayRegister(state, value) {
+      state.displayRegister = value;
+    },
   },
   actions: {
-
+    /**
+     * Call api to get the current user and set store variables
+     *
+     * @param context
+     * @returns {Promise<T>}
+     */
+    getCurrentUser(context) {
+      api.getCurrentUser()
+          .then(response => {
+            context.commit('setUsername', response.name);
+            context.commit('setUserId', response.id);
+            context.dispatch('getGroups');
+            context.dispatch('getTags');
+          })
+          .catch(error => {
+            if (error.response.statusText === 'Unauthorized') {
+              context.commit('setUsername', null);
+              context.commit('setUserId', 0);
+            }
+          });
+    },
   }
 })
