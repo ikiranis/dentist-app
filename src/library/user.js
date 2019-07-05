32e04151e3b897e8f0c1f8c2a88fc4fce513bf0c
rocean <rocean74@gmail.com>
2019-07-05 03:52:17 +0300

    User library

---
+++ src/library/user.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+let user = {
+
+    /**
+     * Set localStorage token data. User logged in
+     *
+     * @param access_token
+     * @param token_type
+     */
+    login(access_token, token_type) {
+        localStorage.accessToken = access_token;
+        localStorage.tokenType = token_type;
+    },
+
+    /**
+     * Delete the token data, to logout the user
+     */
+    logout() {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("tokenType");
+        this.setUserTokenHeader();
+    },
+
+    /**
+     * Set authentication token header to axios
+     */
+    setUserTokenHeader() {
+        // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+        if(typeof localStorage.accessToken !== undefined) {
+            axios.defaults.headers.common['Accept'] = 'application/json';
+            axios.defaults.headers.common['Authorization'] = localStorage.tokenType + " " + localStorage.accessToken;
+        }
+    }
+
+};
+
+export default user;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true; // Display console warning at development mode
+
+// Bootstrap
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// User Authentication
+import user from '@/library/user'
+user.setUserTokenHeader();
+
+// Set routes titles
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next()
+});
 
 new Vue({
   router,
