4cb5bb832a4a66b5ac92cc5f3bb63ce0c74fac6c
rocean <rocean74@gmail.com>
2019-07-06 01:49:36 +0300

    Prettier

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="container my-3">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card card-default">
-          <div class="card-header">About</div>
-          <div class="card-body">
-            <p><strong>Dentist Files</strong> created by <strong><a href="http://apps4net.eu">apps4net</a></strong>
+    <div class="container my-3">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">About</div>
+                    <div class="card-body">
+                        <p><strong>Dentist Files</strong> created by <strong><a href="http://apps4net.eu">apps4net</a></strong>
 
-            <p>Send your feedback at <strong><u>info (at) apps4net.eu</u></strong></p>
+                        <p>Send your feedback at <strong><u>info (at) apps4net.eu</u></strong></p>
 
-          </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({})
-  }
+    export default {
+        data: () => ({})
+    }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,85 +1,85 @@
 <template>
-  <div class="container-fluid mt-3">
+    <div class="container-fluid mt-3">
 
-    <div v-if="!isUserLoggedIn">
+        <div v-if="!isUserLoggedIn">
 
-      <Login v-if="!displayRegister && !username"/>
+            <Login v-if="!displayRegister && !username"/>
 
-      <div class="row">
+            <div class="row">
                 <span class="btn btn-info my-3 ml-auto mr-auto" v-if="!displayRegister && !username"
                       @click="displayRegisterComponent">Register User</span>
-      </div>
+            </div>
+
+        </div>
+
+        <Register v-if="displayRegister"/>
+
+        <div class="row">
+            <span class="btn btn-info my-3 ml-auto mr-auto" v-if="displayRegister" @click="displayLoginComponent">Login User</span>
+        </div>
 
     </div>
-
-    <Register v-if="displayRegister"/>
-
-    <div class="row">
-      <span class="btn btn-info my-3 ml-auto mr-auto" v-if="displayRegister" @click="displayLoginComponent">Login User</span>
-    </div>
-
-  </div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import api from '@/api';
-  import Login from '@/components/users/Login.vue'
-  import Register from '@/components/users/Register.vue'
-  import {mapState, mapMutations} from 'vuex';
+    // @ is an alias to /src
+    import api from '@/api';
+    import Login from '@/components/users/Login.vue'
+    import Register from '@/components/users/Register.vue'
+    import {mapState, mapMutations} from 'vuex';
 
-  // TODO delete localStorage.accessToken if not valid
-  export default {
-    name: 'home',
+    // TODO delete localStorage.accessToken if not valid
+    export default {
+        name: 'home',
 
-    components: {
-      Login,
-      Register
-    },
+        components: {
+            Login,
+            Register
+        },
 
-    data: () => ({
-      isUserLoggedIn: false
-    }),
+        data: () => ({
+            isUserLoggedIn: false
+        }),
 
-    computed: {
-      ...mapState(['displayRegister', 'username']),
+        computed: {
+            ...mapState(['displayRegister', 'username']),
 
-      routeName: function () {
-        return this.$route.name;
-      }
-    },
+            routeName: function () {
+                return this.$route.name;
+            }
+        },
 
-    watch: {
-      username() {
-        // We make sure to take new value of localStorage.accessToken when username is changed
-        this.isUserLoggedIn = !!localStorage.accessToken;
-      }
-    },
+        watch: {
+            username() {
+                // We make sure to take new value of localStorage.accessToken when username is changed
+                this.isUserLoggedIn = !!localStorage.accessToken;
+            }
+        },
 
-    mounted: function() {
-      this.isUserLoggedIn = !!localStorage.accessToken;
-    },
+        mounted: function() {
+            this.isUserLoggedIn = !!localStorage.accessToken;
+        },
 
-    beforeCreate: function () {
-      api.getUsersCount()
-              .then(response => {
-                if (response.count === 0) {
-                  this.setDisplayRegister(true);
-                }
-              })
-              .catch(error => console.log(error.response));
-    },
+        beforeCreate: function () {
+            api.getUsersCount()
+                .then(response => {
+                    if (response.count === 0) {
+                        this.setDisplayRegister(true);
+                    }
+                })
+                .catch(error => console.log(error.response));
+        },
 
-    methods: {
-      ...mapMutations(['setDisplayRegister']),
+        methods: {
+            ...mapMutations(['setDisplayRegister']),
 
-      displayRegisterComponent() {
-        this.setDisplayRegister(true);
-      },
+            displayRegisterComponent() {
+                this.setDisplayRegister(true);
+            },
 
-      displayLoginComponent() {
-        this.setDisplayRegister(false);
-      }
+            displayLoginComponent() {
+                this.setDisplayRegister(false);
+            }
+        }
     }
-  }
 </script>
