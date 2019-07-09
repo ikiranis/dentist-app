6707a6b17cde9e4e1f87b1c86ba5c2cd5c35ca92
rocean <rocean74@gmail.com>
2019-07-10 02:15:26 +0300

    Update dependencies

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.js$': 'babel-jest',
     '^.+\\.jsx?$': 'babel-jest'
   },
   transformIgnorePatterns: [
@@ -22,6 +23,7 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
+  cache: false,
   testURL: 'http://localhost/',
   watchPlugins: [
     'jest-watch-typeahead/filename',
