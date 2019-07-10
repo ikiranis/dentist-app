c0c13946a908c397be67b43e0deac6c5ae23a6e1
rocean <rocean74@gmail.com>
2019-07-11 02:03:03 +0300

    Fix list

--- src/components/economy/FinancialList.vue
+++ src/components/economy/FinancialList.vue
@@ -1,23 +1,23 @@
 <template>
     <table class="table">
         <thead>
-        <tr>
-            <th scope="col">Ημ/νία</th>
-            <th scope="col">Κίνηση</th>
-            <th scope="col">Ποσό</th>
-        </tr>
+            <tr>
+                <th scope="col">Ημ/νία</th>
+                <th scope="col">Κίνηση</th>
+                <th scope="col">Ποσό</th>
+            </tr>
         </thead>
 
-        <tbody v-for="transaction in transactions">
-        <tr v-if="transaction.kind === kind">
-            <th scope="row">{{ transaction.date }}</th>
-            <td>
-                {{ transaction.text }}
-            </td>
-            <td>
-                {{ transaction.value }}
-            </td>
-        </tr>
+        <tbody v-for="transaction in transactions" v-if="transaction.kind === kind">
+            <tr>
+                <th scope="row">{{ transaction.date }}</th>
+                <td>
+                    {{ transaction.text }}
+                </td>
+                <td>
+                    {{ transaction.value }}
+                </td>
+            </tr>
         </tbody>
     </table>
 </template>
