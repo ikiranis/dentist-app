2f6b2d6fb5dddf9e48dcaa3f00e9b7bb566677a7
rocean <rocean74@gmail.com>
2019-07-11 22:53:56 +0300

    Edit text

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,8 +64,8 @@
                 status: ''
             },
             credentials: {
-                username: '',
-                password: ''
+                username: 'rocean@error.gr',
+                password: '123456'
             }
         }),
 
