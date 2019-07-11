f98892f7bac6dcc217a110c40ca31f8e87b403a9
rocean <rocean74@gmail.com>
2019-07-11 18:49:26 +0300

    Edit text

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,9 +7,9 @@
             </div>
 
             <div class="container">
-                <p><strong>Dentist Files</strong> created by <strong><a href="http://apps4net.eu">apps4net</a></strong>
+                <p><strong>Dentist</strong> created by <strong><a href="http://apps4net.eu">apps4net</a></strong>
 
-                <p>Send your feedback at <strong><u>info (at) apps4net.eu</u></strong></p>
+                <p>Send your feedback at <strong><u>info@apps4net.eu</u></strong></p>
             </div>
 
         </div>
