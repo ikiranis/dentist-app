03f0f1787341eb89821533fdbe15e0da63bab602
rocean <rocean74@gmail.com>
2019-07-13 17:58:14 +0300

    ESLINT fix

--- tests/e2e/specs/login.js
+++ tests/e2e/specs/login.js
@@ -1,22 +1,19 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
 
-
 describe('Test Login and logout', () => {
+    it('Load the app', () => {
+        cy.visit('/')
+        cy.contains('div', 'Login')
+    })
 
-  it('Load the app', () => {
-    cy.visit('/')
-    cy.contains('div', 'Login')
-  })
+    it('Login to app', () => {
+        cy.get('#login').click()
+        cy.contains('h1', 'Επιλογές')
+    })
 
-  it('Login to app', () => {
-    cy.get('#login').click()
-    cy.contains('h1', 'Επιλογές')
-  })
-
-  it('Logout', () => {
-    cy.get('#logout').click();
-    cy.contains('div', 'Login1')
-  })
-
+    it('Logout', () => {
+        cy.get('#logout').click()
+        cy.contains('div', 'Login1')
+    })
 })
