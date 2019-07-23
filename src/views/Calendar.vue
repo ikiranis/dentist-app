3bd7dfdb20bf2610ad00ee9922ad6e6b85bd4d89
rocean <rocean74@gmail.com>
2019-07-23 04:10:08 +0300

    Check for empty calendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -55,11 +55,15 @@
                 <h1>Ημερολόγιο</h1>
             </div>
 
-            <div class="container">
+            <div class="alert alert-success text-center w-50 mt-5 mx-auto" v-if="!events.length">
+                Δεν βρέθηκαν ραντεβού
+            </div>
+
+            <div class="container" v-if="events.length">
 
                 <div class="row">
 
-                    <div v-for="event in events" :key="event.date" class="col-4 mb-3">
+                    <div v-for="event in events" :key="event.id" class="col-4 mb-3">
                         <div class="card">
                             <div class="card-header">
                                 {{ event.date }} {{ event.time }}
@@ -77,12 +81,11 @@
 
                 </div>
 
-                <div class="row">
-                    <button class="btn btn-success col-lg-6 col-12 my-3 mx-auto" @click="newEvent()">
-                        Εισαγωγή ραντεβού
-                    </button>
-                </div>
+            </div>
 
+            <div class="row w-100">
+                <input class="btn btn-success col-lg-6 col-12 my-3 mx-auto"
+                       @click="newEvent()" value="Εισαγωγή ραντεβού">
             </div>
         </div>
     </div>
@@ -112,78 +115,91 @@ export default {
 
             events: [
                 {
+                    id: 0,
                     date: '12/01/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 1
                 },
                 {
+                    id: 1,
                     date: '15/01/2019',
                     time: '12:00',
                     description: 'Εξοπλισμός',
                     patient: 5
                 },
                 {
+                    id: 2,
                     date: '12/02/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 2
                 },
                 {
+                    id: 3,
                     date: '14/01/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 4
                 },
                 {
+                    id: 4,
                     date: '18/01/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 3
                 },
                 {
+                    id: 5,
                     date: '14/02/2019',
                     time: '12:00',
                     description: 'Εξοπλισμός',
                     patient: 2
                 },
                 {
+                    id: 6,
                     date: '12/01/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 3
                 },
                 {
+                    id: 7,
                     date: '14/02/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 1
                 },
                 {
+                    id: 8,
                     date: '12/03/2019',
                     time: '12:00',
                     description: 'Εξοπλισμός',
                     patient: 4
                 },
                 {
+                    id: 9,
                     date: '27/03/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 5
                 },
                 {
+                    id: 10,
                     date: '12/05/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
                     patient: 6
                 },
                 {
+                    id: 11,
                     date: '13/01/2019',
                     time: '12:00',
                     description: 'Εξοπλισμός',
                     patient: 5
                 },
                 {
+                    id: 12,
                     date: '22/01/2019',
                     time: '12:00',
                     description: 'Θεραπεία',
@@ -203,7 +219,7 @@ export default {
         },
 
         saveEvent () {
-            //
+            alert('Saving...')
         }
     }
 }
