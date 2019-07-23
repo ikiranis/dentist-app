f29dc46a03ae18a15045816494252b1dd7b02a84
rocean <rocean74@gmail.com>
2019-07-23 04:57:09 +0300

    Styling

--- src/components/patients/EndoTreatment.vue
+++ src/components/patients/EndoTreatment.vue
@@ -12,9 +12,9 @@
             Επιλογή πεδίων
         </div>
 
-        <form class="row col-12 mt-3">
+        <form class="container col-lg-6 col-12 mt-3">
 
-            <div class="input-group row mb-2">
+            <div class="input-group row mb-2 mx-auto">
                 <div class="input-group-prepend">
                     <div class="input-group-text">
                         <label for="chozenRoot" class="my-auto">Ρίζες</label>
@@ -26,41 +26,46 @@
                 </select>
             </div>
 
-            <div v-if="fields[0].display">
-                <span>Μήκος εργαλείου</span>
+            <div class="card mb-2 w-100 mx-auto" v-if="fields[0].display">
+                <div class="card-header">
+                    <span>Μήκος εργαλείου</span>
 
-                <minus-circle-outline class="removeItem my-auto ml-2" title="Αφαίρεση πεδίου"
-                                      @click="removeField(0)"/>
-
-                <div class="input-group row mb-2">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text">
-                            <label for="counter" class="my-auto">Μετρητής</label>
-                        </div>
-                    </div>
-
-                    <input id="counter" type="text" class="form-control"
-                           v-model="endoTreatment.counter" maxlength="10">
-                    <form-error v-if="response.errors.counter"
-                                :error="response.errors.counter[0]"/>
+                    <minus-circle-outline class="removeItem my-auto ml-2" title="Αφαίρεση πεδίου"
+                                          @click="removeField(0)"/>
                 </div>
 
-                <div class="input-group row mb-2">
-                    <div class="input-group-prepend">
-                        <div class="input-group-text">
-                            <label for="radiography" class="my-auto">Ακτινογραφία</label>
+                <div class="card-body">
+
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <label for="counter" class="my-auto">Μετρητής</label>
+                            </div>
                         </div>
+
+                        <input id="counter" type="text" class="form-control"
+                               v-model="endoTreatment.counter" maxlength="10">
+                        <form-error v-if="response.errors.counter"
+                                    :error="response.errors.counter[0]"/>
                     </div>
 
-                    <input id="radiography" type="text" class="form-control"
-                           v-model="endoTreatment.radiography" maxlength="10">
-                    <form-error v-if="response.errors.radiography"
-                                :error="response.errors.radiography[0]"/>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <label for="radiography" class="my-auto">Ακτινογραφία</label>
+                            </div>
+                        </div>
+
+                        <input id="radiography" type="text" class="form-control"
+                               v-model="endoTreatment.radiography" maxlength="10">
+                        <form-error v-if="response.errors.radiography"
+                                    :error="response.errors.radiography[0]"/>
+                    </div>
                 </div>
 
             </div>
 
-            <div class="input-group row mb-2" v-if="fields[1].display">
+            <div class="input-group row mb-2 mx-auto" v-if="fields[1].display">
                 <div class="input-group-prepend">
                     <div class="input-group-text">
                         <label for="workingLength" class="my-auto">Μήκος εργασίας</label>
@@ -75,7 +80,7 @@
                             :error="response.errors.workingLength[0]"/>
             </div>
 
-            <div class="input-group row mb-2" v-if="fields[2].display">
+            <div class="input-group row mb-2 mx-auto" v-if="fields[2].display">
                 <div class="input-group-prepend">
                     <div class="input-group-text">
                         <label for="Benchmark" class="my-auto">Σημείο αναφοράς</label>
@@ -97,7 +102,7 @@
                             :error="response.errors.Benchmark[0]"/>
             </div>
 
-            <div class="input-group row mb-2" v-if="fields[3].display">
+            <div class="input-group row mb-2 mx-auto" v-if="fields[3].display">
                 <div class="input-group-prepend">
                     <div class="input-group-text">
                         <label for="MAF" class="my-auto">MAF</label>
@@ -112,7 +117,7 @@
                             :error="response.errors.MAF[0]"/>
             </div>
 
-            <div class="input-group row mb-2" v-if="fields[4].display">
+            <div class="input-group row mb-2 mx-auto" v-if="fields[4].display">
                 <div class="input-group-prepend">
                     <div class="input-group-text">
                         <label for="chemicalMechanicalTreatment" class="my-auto">Χημικομηχανική επεξεργασία</label>
@@ -127,7 +132,7 @@
                             :error="response.errors.chemicalMechanicalTreatment[0]"/>
             </div>
 
-            <div class="input-group row mb-2" v-if="fields[5].display">
+            <div class="input-group row mb-2 mx-auto" v-if="fields[5].display">
                 <div class="input-group-prepend">
                     <div class="input-group-text">
                         <label for="chozenBlockingTechnique" class="my-auto">Τεχνική έφραξης</label>
@@ -183,7 +188,9 @@
                     </div>
 
                     <div class="row">
-                        <button class="btn btn-success col-lg-6 col-12 my-3 mx-auto" @click="saveNote">Εισαγωγή Σημείωσης</button>
+                        <button class="btn btn-success col-lg-6 col-12 my-3 mx-auto" @click="saveNote">Εισαγωγή
+                            Σημείωσης
+                        </button>
                     </div>
 
                 </form>
@@ -201,208 +208,208 @@
 </template>
 
 <script>
-import FormError from '@/components/basic/FormError'
-import FieldsList from '@/components/patients/FieldsList'
+    import FormError from '@/components/basic/FormError'
+    import FieldsList from '@/components/patients/FieldsList'
 
-export default {
-    components: { FormError, FieldsList },
+    export default {
+        components: {FormError, FieldsList},
 
-    data () {
-        return {
-            response: {
-                message: '',
-                status: '',
-                errors: []
-            },
+        data() {
+            return {
+                response: {
+                    message: '',
+                    status: '',
+                    errors: []
+                },
 
-            fields: [
-                {
+                fields: [
+                    {
+                        id: 0,
+                        label: 'Μήκος εργαλείου',
+                        display: false
+                    },
+                    {
+                        id: 1,
+                        label: 'Μήκος εργασίας',
+                        display: false
+                    },
+                    {
+                        id: 2,
+                        label: 'Σημείο αναφοράς',
+                        display: false
+                    },
+                    {
+                        id: 3,
+                        label: 'MAF',
+                        display: false
+                    },
+                    {
+                        id: 4,
+                        label: 'Χημικομηχανική επεξεργασία',
+                        display: false
+                    },
+                    {
+                        id: 5,
+                        label: 'Τεχνική έμφραξης',
+                        display: false
+                    },
+                    {
+                        id: 6,
+                        label: 'Σημειώσεις',
+                        display: false
+                    }
+                ],
+
+                endoTreatment: {
+                    counter: '',
+                    radiography: '',
+                    workingLength: '',
+                    Benchmark: '',
+                    chozenBenchmark: 0,
+                    MAF: '',
+                    chemicalMechanicalTreatment: '',
+                    chozenBlockingTechnique: 0,
+                    chozenRoot: 0
+                },
+
+                roots: [
+                    {
+                        id: 0,
+                        name: 'Απερώια'
+                    },
+                    {
+                        id: 1,
+                        name: 'Προστομιακή'
+                    },
+                    {
+                        id: 2,
+                        name: 'Προστομιακή εγγύς'
+                    },
+                    {
+                        id: 3,
+                        name: 'Προστομιακή άπω'
+                    },
+                    {
+                        id: 4,
+                        name: '2η προσ. εγγύς'
+                    },
+                    {
+                        id: 5,
+                        name: 'Υπερώια'
+                    },
+                    {
+                        id: 6,
+                        name: 'Άπο γλωσσική'
+                    },
+                    {
+                        id: 7,
+                        name: 'Εγγύς γλωσσική'
+                    },
+                    {
+                        id: 8,
+                        name: 'Άπω προστομιακή'
+                    },
+                    {
+                        id: 9,
+                        name: 'Εγγύς προστομιακή'
+                    }
+                ],
+
+                benchmarks: [
+                    {
+                        id: 0,
+                        name: 'Κοπτικό χείλος'
+                    },
+                    {
+                        id: 1,
+                        name: 'Παρειακό φύμα'
+                    },
+                    {
+                        id: 2,
+                        name: 'Υπερώιο φύμα'
+                    },
+                    {
+                        id: 3,
+                        name: 'Εγγύς παρειακό'
+                    },
+                    {
+                        id: 4,
+                        name: 'Άπω παρειακό'
+                    },
+                    {
+                        id: 5,
+                        name: 'Εγγύς γλωσσικό'
+                    },
+                    {
+                        id: 6,
+                        name: 'Άπω γλωσσικό'
+                    },
+                    {
+                        id: 7,
+                        name: 'Εγγύς υπερώιο'
+                    },
+                    {
+                        id: 8,
+                        name: 'Άπω υπερώιο'
+                    }
+                ],
+
+                notes: [
+                    {
+                        id: 0,
+                        date: '12/01/2019',
+                        description: 'something'
+                    },
+                    {
+                        id: 1,
+                        date: '22/01/2019',
+                        description: 'something about something'
+                    },
+                    {
+                        id: 2,
+                        date: '13/02/2019',
+                        description: 'something for something'
+                    },
+                    {
+                        id: 3,
+                        date: '25/03/2019',
+                        description: 'somethings about somethings'
+                    }
+                ],
+
+                note: {
                     id: 0,
-                    label: 'Μήκος εργαλείου',
-                    display: false
-                },
-                {
-                    id: 1,
-                    label: 'Μήκος εργασίας',
-                    display: false
-                },
-                {
-                    id: 2,
-                    label: 'Σημείο αναφοράς',
-                    display: false
-                },
-                {
-                    id: 3,
-                    label: 'MAF',
-                    display: false
-                },
-                {
-                    id: 4,
-                    label: 'Χημικομηχανική επεξεργασία',
-                    display: false
-                },
-                {
-                    id: 5,
-                    label: 'Τεχνική έμφραξης',
-                    display: false
-                },
-                {
-                    id: 6,
-                    label: 'Σημειώσεις',
-                    display: false
+                    date: '',
+                    description: ''
                 }
-            ],
 
-            endoTreatment: {
-                counter: '',
-                radiography: '',
-                workingLength: '',
-                Benchmark: '',
-                chozenBenchmark: 0,
-                MAF: '',
-                chemicalMechanicalTreatment: '',
-                chozenBlockingTechnique: 0,
-                chozenRoot: 0
-            },
-
-            roots: [
-                {
-                    id: 0,
-                    name: 'Απερώια'
-                },
-                {
-                    id: 1,
-                    name: 'Προστομιακή'
-                },
-                {
-                    id: 2,
-                    name: 'Προστομιακή εγγύς'
-                },
-                {
-                    id: 3,
-                    name: 'Προστομιακή άπω'
-                },
-                {
-                    id: 4,
-                    name: '2η προσ. εγγύς'
-                },
-                {
-                    id: 5,
-                    name: 'Υπερώια'
-                },
-                {
-                    id: 6,
-                    name: 'Άπο γλωσσική'
-                },
-                {
-                    id: 7,
-                    name: 'Εγγύς γλωσσική'
-                },
-                {
-                    id: 8,
-                    name: 'Άπω προστομιακή'
-                },
-                {
-                    id: 9,
-                    name: 'Εγγύς προστομιακή'
-                }
-            ],
-
-            benchmarks: [
-                {
-                    id: 0,
-                    name: 'Κοπτικό χείλος'
-                },
-                {
-                    id: 1,
-                    name: 'Παρειακό φύμα'
-                },
-                {
-                    id: 2,
-                    name: 'Υπερώιο φύμα'
-                },
-                {
-                    id: 3,
-                    name: 'Εγγύς παρειακό'
-                },
-                {
-                    id: 4,
-                    name: 'Άπω παρειακό'
-                },
-                {
-                    id: 5,
-                    name: 'Εγγύς γλωσσικό'
-                },
-                {
-                    id: 6,
-                    name: 'Άπω γλωσσικό'
-                },
-                {
-                    id: 7,
-                    name: 'Εγγύς υπερώιο'
-                },
-                {
-                    id: 8,
-                    name: 'Άπω υπερώιο'
-                }
-            ],
-
-            notes: [
-                {
-                    id: 0,
-                    date: '12/01/2019',
-                    description: 'something'
-                },
-                {
-                    id: 1,
-                    date: '22/01/2019',
-                    description: 'something about something'
-                },
-                {
-                    id: 2,
-                    date: '13/02/2019',
-                    description: 'something for something'
-                },
-                {
-                    id: 3,
-                    date: '25/03/2019',
-                    description: 'somethings about somethings'
-                }
-            ],
-
-            note: {
-                id: 0,
-                date: '',
-                description: ''
             }
+        },
 
-        }
-    },
+        computed: {
+            fieldSelected() {
+                return this.fields.find((field) => {
+                    return field.display;
+                })
+            }
+        },
 
-    computed: {
-        fieldSelected () {
-            return this.fields.find((field) => {
-                return field.display;
-            })
-        }
-    },
+        methods: {
 
-    methods: {
-
-        /**
+            /**
              * Εξαφάνιση του πεδίου
              *
              * @param field
              */
-        removeField (field) {
-            this.fields[field].display = false
-        },
+            removeField(field) {
+                this.fields[field].display = false
+            },
 
-        saveNote () {
-            alert('Saving...')
+            saveNote() {
+                alert('Saving...')
+            }
         }
     }
-}
 </script>
 
 <style lang="scss" scoped>
