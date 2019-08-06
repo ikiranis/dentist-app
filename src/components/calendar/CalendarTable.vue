9bc0c5719bbf5a7c960ae6b242ec23f78b5de02b
rocean <rocean74@gmail.com>
2019-08-06 03:18:30 +0300

    Display calendar data

---
+++ src/components/calendar/CalendarTable.vue
@@ -0,0 +1,34 @@
+<template functional>
+    <div class="row">
+        <div v-for="event in props.events" :key="event.id" class="col-4 mb-3">
+            <div class="card">
+                <div class="card-header">
+                    <span>{{ event.date }} {{ event.time }}</span>
+
+                    <delete @click="listeners.click(event.id)" title="Διαγραφή ραντεβού"/>
+                </div>
+
+                <div class="card-body">
+                    {{ event.description }}
+                </div>
+
+                <div class="card-footer">
+                    {{ event.patient }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CalendarTable',
+
+        props: {
+            events: {
+                required: true,
+                type: Array
+            }
+        }
+    }
+</script>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -55,29 +55,22 @@
                 <h1>Ημερολόγιο</h1>
             </div>
 
-            <div class="alert alert-success text-center w-50 mt-5 mx-auto" v-if="!events.length">
-                Δεν βρέθηκαν ραντεβού
-            </div>
+            <Loading :loading="loading"/>
 
-            <div class="container" v-if="events.length">
+            <div v-if="!loading">
 
-                <div class="row">
+                <div class="alert alert-success text-center w-50 mt-5 mx-auto"
+                     v-if="!events.length && !loading">
+                    Δεν βρέθηκαν ραντεβού
+                </div>
 
-                    <div v-for="event in events" :key="event.id" class="col-4 mb-3">
-                        <div class="card">
-                            <div class="card-header">
-                                {{ event.date }} {{ event.time }}
-                            </div>
+                <div class="container" v-if="events.length && !loading">
 
-                            <div class="card-body">
-                                {{ event.description }}
-                            </div>
+                    <paginate :pagination="pagination" @click="getEvents"/>
 
-                            <div class="card-footer">
-                                {{ event.patient }}
-                            </div>
-                        </div>
-                    </div>
+                    <calendar-table :events="events" @click="deleteEvent" />
+
+                    <paginate :pagination="pagination" @click="getEvents"/>
 
                 </div>
 
@@ -88,139 +81,138 @@
                        @click="newEvent()" value="Εισαγωγή ραντεβού">
             </div>
         </div>
+
+        <display-error v-if="response.message" :response="response"/>
+
     </div>
 </template>
 
 <script>
-import FormError from '@/components/basic/FormError'
+    import api from '@/api'
+    import FormError from '@/components/basic/FormError'
+    import DisplayError from '@/components/basic/DisplayError'
+    import Loading from '@/components/basic/Loading'
+    import utility from "../library/utility";
+    import Paginate from '@/components/basic/Paginate'
+    import CalendarTable from '@/components/calendar/CalendarTable'
 
-export default {
-    components: { FormError },
+    export default {
+        components: { FormError, Loading, DisplayError, Paginate, CalendarTable },
 
-    data () {
-        return {
+        data() {
+            return {
 
-            response: {
-                message: '',
-                status: '',
-                errors: []
-            },
+                response: {
+                    message: '',
+                    status: '',
+                    errors: []
+                },
 
-            event: {
-                date: '',
-                time: '',
-                description: '',
-                patient: 0
-            },
+                loading: false,
 
-            events: [
-                {
-                    id: 0,
-                    date: '12/01/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 1
-                },
-                {
-                    id: 1,
-                    date: '15/01/2019',
-                    time: '12:00',
-                    description: 'Εξοπλισμός',
-                    patient: 5
-                },
-                {
-                    id: 2,
-                    date: '12/02/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 2
-                },
-                {
-                    id: 3,
-                    date: '14/01/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 4
-                },
-                {
-                    id: 4,
-                    date: '18/01/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 3
-                },
-                {
-                    id: 5,
-                    date: '14/02/2019',
-                    time: '12:00',
-                    description: 'Εξοπλισμός',
-                    patient: 2
-                },
-                {
-                    id: 6,
-                    date: '12/01/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 3
-                },
-                {
-                    id: 7,
-                    date: '14/02/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 1
-                },
-                {
-                    id: 8,
-                    date: '12/03/2019',
-                    time: '12:00',
-                    description: 'Εξοπλισμός',
-                    patient: 4
-                },
-                {
-                    id: 9,
-                    date: '27/03/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 5
-                },
-                {
-                    id: 10,
-                    date: '12/05/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 6
-                },
-                {
-                    id: 11,
-                    date: '13/01/2019',
-                    time: '12:00',
-                    description: 'Εξοπλισμός',
-                    patient: 5
-                },
-                {
-                    id: 12,
-                    date: '22/01/2019',
-                    time: '12:00',
-                    description: 'Θεραπεία',
-                    patient: 7
-                }
-            ]
-        }
-    },
+                search: '',
 
-    methods: {
+                event: {
+                    date: '',
+                    time: '',
+                    description: '',
+                    patient: 0
+                },
 
-        /**
-             * Display event modal
-             */
-        newEvent () {
-            this.$refs.eventModal.show()
+                pagination: {
+                    meta: {},
+                    links: {}
+                },
+
+                // events: [
+                //     {
+                //         id: 0,
+                //         date: '12/01/2019',
+                //         time: '12:00',
+                //         description: 'Θεραπεία',
+                //         patient: 1
+                //     },
+
+                events: []
+            }
         },
 
-        saveEvent () {
-            alert('Saving...')
+        created: function () {
+            this.getEvents(null)
+        },
+
+        methods: {
+
+            /**
+             * Get all the events
+             *
+             * @param page
+             */
+            getEvents(page) {
+                this.loading = true
+
+                api.getEvents(page, this.search)
+                    .then(response => {
+                        this.loading = false
+
+                        if (response.status === 200) {
+                            this.events = response.data.data
+                            this.pagination.meta = response.data.meta
+                            this.pagination.links = response.data.links
+
+                            window.scrollTo(0, 0)
+
+                            return
+                        }
+
+                        this.events = []
+                    })
+                    .catch(error => {
+                        this.loading = false
+
+                        this.response.message = error.response.data.message
+                        this.response.status = false
+
+                        utility.debug(error.response.data.debug)
+                    })
+            },
+
+            /**
+             * Delete an event
+             */
+            deleteEvent(eventId) {
+                let choise = confirm('Θέλεις σίγουρα να σβήσεις το ραντεβού με id: ' + eventId + ';')
+
+                this.loading = true
+
+                if (choise) {
+                    api.deleteEvent(eventId)
+                        .then(response => {
+                            this.loading = false
+
+                            this.getEvents(null)
+                        })
+                        .catch(error => {
+                            this.loading = false
+
+                            this.response.message = error.response.data.message
+                            this.response.status = false
+
+                            utility.debug(error.response.data.debug)
+                        })
+                }
+            },
+
+            /**
+             * Display event modal
+             */
+            newEvent() {
+                this.$refs.eventModal.show()
+            },
+
+            saveEvent() {
+                alert('Saving...')
+            }
         }
     }
-}
 </script>
