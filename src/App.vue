7ff7352a7d85aa1fd791a138080c61436539a6f2
rocean <rocean74@gmail.com>
2019-09-10 23:25:00 +0300

    Styling

--- src/App.vue
+++ src/App.vue
@@ -115,11 +115,11 @@ export default {
 
     input[type=checkbox] {
         /* Double-sized Checkboxes */
-        -ms-transform: scale(2); /* IE */
-        -moz-transform: scale(2); /* FF */
-        -webkit-transform: scale(2); /* Safari and Chrome */
-        -o-transform: scale(2); /* Opera */
-        padding: 5px;
+        -ms-transform: scale(1.2); /* IE */
+        -moz-transform: scale(1.2); /* FF */
+        -webkit-transform: scale(1.2); /* Safari and Chrome */
+        -o-transform: scale(1.2); /* Opera */
+        /*padding: 10px;*/
     }
 
     .removeItem {
--- src/components/patients/FilesList.vue
+++ src/components/patients/FilesList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <table class="table table-hover">
+        <table class="table table-hover table-responsive">
             <thead>
             <tr>
                 <th scope="col"></th>
